01e182fffc9b8f4d11e966ebf81d18a101140727
Gauthier Flichy <ga2015@flichy.biz>
2016-05-10 21:57:33 +0200

    Ajout de la fonction de listing des Public rooms (commentée par défaut)

--- functions.fn.php
+++ functions.fn.php
@@ -1,13 +1,15 @@
 <?php
 
+/* Login in Rocket Chat */
+
 function RocketChatLogin($user, $mdp){
     $postData = '';
     $url = 'https://demo.rocket.chat/api/login';
     $curl = curl_init($url);
 
     $curl_post_data = array(
-        'password' => '%9T1B3$oqv4e',
-        'user' => 'Flichy'
+        'password' => $mdp,
+        'user' => $user
     );
 
     foreach($curl_post_data as $k => $v)
@@ -26,6 +28,41 @@ function RocketChatLogin($user, $mdp){
 
     $curl_response = json_decode(curl_exec($curl));
 
-    echo 'Login successful !';
-    return $curl_response;
+    curl_close($curl);
+
+    if($curl_response->status === "success"){
+        echo 'Login successful !';
+        return $curl_response;
+    } else {
+        echo "Login failed !";
+    }
+
+}
+
+
+/* Get Public Rooms in RocketChat */
+
+function RocketChatListRooms($authToken, $userId){
+
+    $url = 'https://demo.rocket.chat/api/publicRooms';
+
+    $curl = curl_init();
+
+    curl_setopt($curl,CURLOPT_URL, $url);
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl,CURLOPT_HTTPHEADER,array("X-Auth-Token: ".$authToken, "X-User-Id: ".$userId));
+
+    $curl_response = json_decode(curl_exec($curl));
+
+    //var_dump($curl_response->rooms->_id);
+
+    if($curl_response->status === "success"){
+        echo 'Request successful !';
+        return $curl_response;
+    } else {
+        echo "Request failed !";
+    }
+
+    curl_close($curl);
 }
--- sendIMNotification.php
+++ sendIMNotification.php
@@ -5,6 +5,16 @@ require 'functions.fn.php';
 $user = 'Flichy';
 $mdp = '%9T1B3$oqv4e';
 
-$connection = RocketChatLogin($user, $mdp);                  // Login function with password and user as parameters
+$connection = RocketChatLogin($user, $mdp);                                                         // Login function with user and password as parameters
+
+echo "\n\nAuthtoken: ".$connection->data->authToken."\n";
+echo "userId: ".$connection->data->userId."\n";
+
+
+// Uncomment to use it - (it takes a while to list all rooms)
+
+    //$publicRoomsList = RocketChatListRooms($connection->data->authToken, $connection->data->userId);    // Returns an object containing all public rooms and their infos
+
+    //var_dump($publicRoomsList);
 
 
